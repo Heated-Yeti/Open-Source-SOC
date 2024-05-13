c11e6c63c28ce7ce45d9658f4cdac4e773656b89
Heated-Yeti <23yatharth@gmail.com>
2024-05-13 17:31:43 +0530

    feed enable misp

--- misp_install.sh
+++ misp_install.sh
@@ -34,16 +34,16 @@ api_key=$(grep "^ADMIN_KEY=" ./.env | cut -d "=" -f 2)
 wget https://raw.githubusercontent.com/MISP/MISP/2.4/app/files/feed-metadata/defaults.json
 
 # waiting for container to start
-for ((i=15;i>=0;i--)){
+for ((i=120;i>=0;i--)){
 cat << EOF
 ===========================================================
 
-Waiting for $i seconds  
+Waiting for container to start, $i seconds 
 
 ===========================================================
 EOF
 sleep 1s
-clear 
+clear
 }
 
 # adding feeds to misp (defaults.json used with /feeds/importFeeds/ but api present for /feeds/add)
@@ -51,8 +51,11 @@ json_payload=$(cat defaults.json)
 response=$(curl -XPOST --insecure --header "Authorization: $api_key" --header "Accept: application/json" --header "Content-Type: application/json" -d "${json_payload}" "https://localhost/feeds/importFeeds")
 echo "Response: $response"
 
+# marking all feeds as enabled
+curl -XPOST --insecure --header "Authorization: $api_key" --header "Accept: application/json" --header "Content-Type: text/html" "https://localhost/feeds/toggleSelected/1/0/%5B1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20%5D"
 
 # misp cron job to automatically update feeds daily (to change the period by use crontab)
-(crontab -l ; echo "0 1 * * * /usr/bin/curl -XPOST --insecure --header "Authorization: $api_key" --header "Accept: application/json" --header "Content-Type: application/json" https://localhost:80/feeds/fetchFromAllFeeds") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+curl -XPOST --insecure --header "Authorization: $api_key" --header "Accept: application/json" --header "Content-Type: application/json" https://localhost/feeds/fetchFromAllFeeds
+(crontab -l ; echo "0 1 * * * /usr/bin/curl -XPOST --insecure --header "Authorization: $api_key" --header "Accept: application/json" --header "Content-Type: application/json" https://localhost/feeds/fetchFromAllFeeds") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
 
 cd ..
